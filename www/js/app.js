03b35be796785579619b1f981fde020a09b5fa62
gytis <gytis@redhat.com>
2016-01-24 14:02:43 +0100

    Create cards and store locally

--- www/js/app.js
+++ www/js/app.js
@@ -3,22 +3,52 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('flashCards', ['ionic'])
+angular.module('flashCards', ['ionic', 'utils'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .controller('FlashCardsController', function($scope, $ionicModal, $localCardsContainer) {
+    $scope.cards = $localCardsContainer.getAll();
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicModal.fromTemplateUrl('new-card.html', function(modal) {
+      $scope.cardModal = modal;
+    }, {
+      scope: $scope,
+      animation: 'slide-in-up'
+    });
+
+    $scope.createCard = function(card) {
+      $localCardsContainer.add({
+        front: card.front,
+        back: card.back
+      });
+      $scope.cards = $localCardsContainer.getAll();
+      $scope.cardModal.hide();
+      card.front = '';
+      card.back = '';
+    };
+
+    $scope.newCard = function() {
+      $scope.cardModal.show();
+    };
+
+    $scope.closeNewCard = function() {
+      $scope.cardModal.hide();
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  })
+
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
   });
-})
